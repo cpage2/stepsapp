a843c18de75ef7b1b056bfcf654fff83bcdab2da
cpage2 <mcpage25@gmail.com>
2023-06-20 19:38:17 -0500

    input text

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="top">
+      <p id='logo'>STEPS</p>
+
+      <div className='menu'>
+          <a href=''>About</a>
+          <br></br>
+          <a href=''>Shop</a>
+      </div>
+   
     </div>
   );
 }
 
 export default App;
+
+
+//menu (hamburger - logo - about - shop - cart button)
+//statement
+//images
