be78d7043c6cd48c4cc0f32271da7748e7d9fb4c
cpage2 <mcpage25@gmail.com>
2023-06-20 19:43:12 -0500

    change about/shop to list

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="top">
-      <p id='logo'>STEPS</p>
+       <p id='logo'>STEPS</p>
 
-      <div className='menu'>
-          <a href=''>About</a>
-          <br></br>
-          <a href=''>Shop</a>
-      </div>
+      
+        <ul className = "menu">
+          <li><a href=''>About</a></li>
+          <li><a href=''>Shop</a></li>
+        </ul>
+      
    
     </div>
   );
