7c26636183a6b041984c7045eb82207a5f081238
cpage2 <mcpage25@gmail.com>
2023-06-22 13:16:06 -0500

    added img imports to react and assets to page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 
 import './App.css';
+import statementImg from './assets/statement.png';
+import shoeOne from './assets/frontendfriday-shoe 1.png';
+import shoeTwo from './assets/pexels-ray-piedra-1464625 1.png';
 
 function App() {
-  return (
-    <div className="top">
-       <p id='logo'>STEPS</p>
 
-      
-        <ul className = "menu">
-          <li><a href=''>About</a></li>
-          <li><a href=''>Shop</a></li>
-        </ul>
-      
-   
+
+
+
+  return (
+    <><div className="top">
+      <p id='logo' style={{color: "white"}}>STEPS</p>
+
+
+      <ul className="menu">
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><button>Cart</button></li>
+      </ul>
+
     </div>
+    
+    <div className='statement'>
+          <img src={statementImg}alt='img'/>
+
+      </div>
+
+      <div className='shoes'>
+         <img src={shoeOne} alt='Shoe'/>
+         <img src = {shoeTwo} alt= 'Shoe 2'/>
+      </div>
+      
+      </>
   );
 }
 
